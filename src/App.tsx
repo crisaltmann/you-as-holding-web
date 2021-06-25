cc2347aa49fa0f973fce1f3081fd34506bf9be52
Cristiano Altmann <crisaltmann@gmail.com>
2021-06-25 16:44:47 -0300

    mudar logica do router para funcionalidade no ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 import Login from "./containers/Login";
 
 import { Main } from "./containers/Main";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path='/' exact={true} component={Main} />
         <Route path='/login' component={Login} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
